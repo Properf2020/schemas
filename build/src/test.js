879570fb3562dfb05a22e38c6f78627f94739846
Guillaume <iambluedev@gmx.fr>
2020-10-16 00:02:53 +0200

    fix compilation

--- build/src/test.js
+++ build/src/test.js
@@ -1,7 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const entities_1 = require("./entities");
-const enums_1 = require("./enums");
 const index_1 = require("./index");
 const createBill = (price, club, paymentStatus) => {
     return new entities_1.Bill(new Date(), new Date(), price, club, paymentStatus);
@@ -9,8 +8,8 @@ const createBill = (price, club, paymentStatus) => {
 const createClub = (name, idffn) => {
     return new entities_1.Club(name, idffn);
 };
-const createSwimmer = (firstName, lastName, birthDate, club, sex) => {
-    return new entities_1.Swimmer(firstName, lastName, birthDate, club, sex);
+const createSwimmer = (firstName, lastName, birthDate, sex) => {
+    return new entities_1.Swimmer(firstName, lastName, birthDate, sex);
 };
 (async () => {
     const DI = await index_1.createConnection({
@@ -18,18 +17,4 @@ const createSwimmer = (firstName, lastName, birthDate, club, sex) => {
         mongoName: "properf-test"
     });
     const em = DI.em.fork();
-    const club1 = createClub("Mon club de ouf", 1);
-    const club2 = createClub("Mon club nul", 2);
-    const bill = createBill(1500, club1, enums_1.EPaymentStatus.PENDING);
-    let swimmer = createSwimmer("Test", "John", new Date(), club1, "H");
-    swimmer.idFFn = "1";
-    em.persistAndFlush([club1, club2, bill, swimmer]);
-    const retrieved = await em.getRepository(entities_1.Club).findOne({ idFfn: 1 }, ['bills']);
-    if (retrieved == undefined)
-        throw new Error("Aucunes bills de save c'est bizarre");
-    const bills = retrieved.bills.getItems();
-    console.log("On nous doit encore " + bills
-        .filter(bill => bill.paymentStatus == enums_1.EPaymentStatus.PENDING)
-        .map(bill => bill.price)
-        .reduce((prev, bill) => prev + bill) + " $");
 })();
--- src/test.ts
+++ src/test.ts
@@ -1,3 +1,4 @@
+import { EntityManager } from '@mikro-orm/core';
 import { Bill, Club, Swimmer } from './entities';
 import { EPaymentStatus } from './enums';
 import { createConnection } from './index'
@@ -10,8 +11,8 @@ const createClub = (name: string, idffn: number): Club => {
     return new Club(name, idffn);
 }
 
-const createSwimmer = (firstName: string, lastName: string, birthDate: Date, club: Club, sex: string): Swimmer => {
-    return new Swimmer(firstName, lastName, birthDate, club, sex);
+const createSwimmer = (firstName: string, lastName: string, birthDate: Date, sex: string): Swimmer => {
+    return new Swimmer(firstName, lastName, birthDate, sex);
 }
 
 (async () => {
@@ -21,8 +22,38 @@ const createSwimmer = (firstName: string, lastName: string, birthDate: Date, clu
     });
 
     const em = DI.em.fork();
+    /*const club1 = createClub("Mon club de ouf", 1);
+    let swimmer = createSwimmer(
+        "Test",
+        "John",
+        new Date(),
+        "H"
+    );
+    swimmer.idFFn = "1";
+    swimmer.clubs.add(club1);
+    em.persistAndFlush([swimmer]);
+    console.log(club1.swimmers)
+    const linkSwimmersToClub = async (em: EntityManager, club: Club): Promise<void> => {
+        const entity = em.fork();
+        console.log("> " + club.name);
+        return new Promise(async (res) => {
+            const swimmers = await em.getRepository<Swimmer>(Swimmer).find({
+                club
+            });
 
-    const club1 = createClub("Mon club de ouf", 1);
+            for await (const swimmer of swimmers)
+                if (!club.swimmers.contains(swimmer))
+                    club.swimmers.add(swimmer);
+
+            await entity.persistAndFlush(club);
+            res();
+        })
+    }
+
+    for await (const club of (await em.getRepository<Club>(Club).find({}, { limit: 100, offset: 1000 })))
+        await linkSwimmersToClub(em, club);*/
+
+    /*const club1 = createClub("Mon club de ouf", 1);
     const club2 = createClub("Mon club nul", 2);
     const bill = createBill(1500, club1, EPaymentStatus.PENDING);
     let swimmer = createSwimmer(
@@ -33,16 +64,17 @@ const createSwimmer = (firstName: string, lastName: string, birthDate: Date, clu
         "H"
     );
     swimmer.idFFn = "1";
-
+ 
     em.persistAndFlush([club1, club2, bill, swimmer]);
-
+ 
     const retrieved = await em.getRepository<Club>(Club).findOne({ idFfn: 1 }, ['bills'])
     if (retrieved == undefined)
         throw new Error("Aucunes bills de save c'est bizarre");
-
+ 
     const bills: Bill[] = retrieved.bills.getItems();
     console.log("On nous doit encore " + bills
         .filter(bill => bill.paymentStatus == EPaymentStatus.PENDING)
         .map(bill => bill.price)
-        .reduce((prev, bill) => prev + bill) + " $")
+        .reduce((prev, bill) => prev + bill) + " $")*/
+
 })();
