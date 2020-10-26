0a51b3db8093140ffef394ea74687828f07838ef
Guillaume <iambluedev@gmx.fr>
2020-10-26 23:12:26 +0100

    update exports

--- src/schemas/index.js
+++ src/schemas/index.js
@@ -1,12 +1,14 @@
-export * from './Club'
-export * from './ClubRaw'
-export * from './Course'
-export * from './CourseRaw'
-export * from './StatsClub'
-export * from './StatsCountry'
-export * from './StatsDepartement'
-export * from './StatsRegion'
-export * from './Swimmer'
-export * from './SwimmerMarge'
-export * from './SwimmerRecord'
-export * from './User'
+module.exports = {
+    Club: require('./Club'),
+    ClubRaw: require('./ClubRaw'),
+    Course: require('./Course'),
+    CourseRaw: require('./CourseRaw'),
+    StatsClub: require('./StatsClub'),
+    StatsCountry: require('./StatsCountry'),
+    StatsDepartement: require('./StatsDepartement'),
+    StatsRegion: require('./StatsRegion'),
+    Swimmer: require('./Swimmer'),
+    SwimmerMarge: require('./SwimmerMarge'),
+    SwimmerRecord: require('./SwimmerRecord'),
+    User: require('./User'),
+}
