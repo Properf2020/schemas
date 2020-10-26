1d38f75d4f5bcf79f4c54344f764722d6fedba5f
Guillaume <iambluedev@gmx.fr>
2020-10-26 23:10:32 +0100

    auto load schemas

--- src/connection.js
+++ src/connection.js
@@ -1,8 +1,15 @@
 const mongoose = require('mongoose');
+const fs = require("fs");
 
 module.exports.createConnection = (host, database) => {
     return new Promise((resolve, reject) => {
         mongoose.connect('mongodb://' + host + ':27017/' + database, { useNewUrlParser: true, useUnifiedTopology: true });
+
+        fs.readdirSync(__dirname + "/schemas").forEach((file) => {
+            if (file == "index.js") return;
+            let model = require(__dirname + "/schemas/" + file);
+            console.log("Loaded model " + file);
+        });
 
         var db = mongoose.connection;
         db.on('error', function (err) {
