b9270370beeb2abed4b1bf96b29903d4d83baf89
Bilel Banelhaq <bilel.banelhaq@eisti.fr>
2020-12-23 23:46:59 +0100

    adding statsbynotes for club

--- src/schemas/StatsClub.js
+++ src/schemas/StatsClub.js
@@ -135,6 +135,32 @@ const statsClubSchema = new mongoose.Schema({
       currentVarianceMarges: Number,
     },
   ],
+  statsByNote: [
+    {
+      note: Number,
+      numCourses: Number,
+      moyennePerf: Number,
+      maxPerf: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'course',
+      },
+      minPerf: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'course',
+      },
+      variancePerf: Number,
+      moyenneMarges: Number,
+      maxMarges: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'swimmer_marge',
+      },
+      minMarges: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'swimmer_marge',
+      },
+      varianceMarges: Number,
+    },
+  ],
 });
 
 statsClubSchema.pre(/^find/, function (next) {
