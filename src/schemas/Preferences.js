7f4adb8ab738917e3ad4a6b6e41cfa6bcb3ea286
Bilel Banelhaq <bilel.banelhaq@gmail.com>
2021-03-20 19:15:00 +0100

    Preferences schema improved

--- src/schemas/Preferences.js
+++ src/schemas/Preferences.js
@@ -1,5 +1,54 @@
 const mongoose = require('mongoose');
 
+const ressentiSchema = new mongoose.Schema({ question: String });
+const periodeSchema = new mongoose.Schema({
+  name: String,
+  dateStart: Date,
+  dateEnd: Date,
+});
+const bilanSchema = new mongoose.Schema({
+  name: String,
+  variable: {
+    type: mongoose.Schema.Types.Mixed,
+  },
+  methode: { type: String, enum: ['diag', 'cumul', 'avg'] },
+});
+const ensembleSchema = new mongoose.Schema({
+  name: String,
+});
+const seanceSchema = new mongoose.Schema({
+  name: String,
+  ensemble: {
+    type: mongoose.Schema.Types.Mixed,
+  },
+  blocks: [
+    {
+      sequences: [
+        {
+          variables: [
+            {
+              variable: {
+                type: mongoose.Schema.Types.Mixed,
+              },
+              defaultValue: String,
+            },
+          ],
+        },
+      ],
+      rep: Number,
+    },
+  ],
+});
+const variableSchema = new mongoose.Schema({
+  name: String,
+  type: {
+    type: String,
+    enum: ['number', 'text', 'boolean', 'arrayNumber', 'arrayText', 'varComp'],
+  },
+  arrayValues: [mongoose.Schema.Types.Mixed],
+  variablesComp: [mongoose.Schema.Types.Mixed],
+});
+
 const preferenceSchema = new mongoose.Schema({
   user: {
     type: mongoose.Schema.ObjectId,
@@ -13,79 +62,12 @@ const preferenceSchema = new mongoose.Schema({
     required: [true, 'Une préférences doit avoir un club'],
     index: true,
   },
-  ressenti: [{ type: String, unique: true }],
-  periodes: [{ name: String, dateStart: Date, dateEnd: Date }],
-  bilans: [
-    {
-      name: String,
-      variable: { type: mongoose.Schema.ObjectId, ref: 'preference.variables' },
-      methode: { type: String, enum: ['diag', 'cumul', 'avg'] },
-    },
-  ],
-  ensembles: [
-    {
-      name: String,
-      seances: [{ type: mongoose.Schema.ObjectId, ref: 'preference.seances' }],
-    },
-  ],
-  seances: [
-    {
-      name: String,
-      ensemble: { type: mongoose.Schema.ObjectId, ref: 'preference.ensembles' },
-      blocks: [
-        {
-          variables: [
-            {
-              variable: {
-                type: mongoose.Schema.ObjectId,
-                ref: 'preference.variables',
-              },
-              defaultValue: String,
-            },
-          ],
-          rep: Number,
-        },
-      ],
-    },
-  ],
-  variables: [
-    {
-      name: String,
-      type: {
-        type: String,
-        enum: [
-          'number',
-          'text',
-          'boolean',
-          'arrayNumber',
-          'arrayText',
-          'varComp',
-        ],
-      },
-      arrayValues: [mongoose.Schema.Types.Mixed],
-      variablesComp: [mongoose.Schema.Types.Mixed],
-    },
-  ],
-});
-
-preferenceSchema.pre(/^find/, function (next) {
-  this.populate({
-    path: 'seances.ensemble',
-    model: 'preference',
-    select: 'name',
-  });
-  this.populate({
-    path: 'seances.blocks.variables.variable',
-    model: 'preference',
-    select: 'name arrayValues',
-  });
-  this.populate({
-    path: 'bilans.variables',
-    model: 'preference',
-    select: 'name arrayValues',
-  });
-
-  next();
+  ressenti: [ressentiSchema],
+  periodes: [periodeSchema],
+  bilans: [bilanSchema],
+  ensembles: [ensembleSchema],
+  seances: [seanceSchema],
+  variables: [variableSchema],
 });
 
 const Preference = mongoose.model('preference', preferenceSchema);
